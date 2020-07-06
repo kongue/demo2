423cfeb564b162402c51c09e7c2a203057cd2bc3
kongue <sunshine105@sina.com>
2020-07-06 14:35:01 +0800

    初始化

---
+++ src/main/java/com/p/demo2/Demo2Application.java
@@ -0,0 +1,19 @@
+package com.p.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Demo2Application {
+	@GetMapping("/test")
+	public String test(){
+		return "demo2";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+}
